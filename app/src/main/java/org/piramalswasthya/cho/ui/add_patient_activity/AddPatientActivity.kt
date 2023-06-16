e36431d0879e9c75d86a7f85f481b3c6a11bf7ae
pauldibyendu <paul.d@beehyv.com>
2023-06-15 22:29:24 -0700

    add patient activity added

---
+++ app/src/main/java/org/piramalswasthya/cho/ui/add_patient_activity/AddPatientActivity.kt
@@ -0,0 +1,12 @@
+package org.piramalswasthya.cho.ui.add_patient_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.piramalswasthya.cho.R
+
+class AddPatientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_patient)
+    }
+}
--- app/src/main/java/org/piramalswasthya/cho/ui/home_activity/HomeActivity.kt
+++ app/src/main/java/org/piramalswasthya/cho/ui/home_activity/HomeActivity.kt
@@ -1,12 +1,43 @@
 package org.piramalswasthya.cho.ui.home_activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import org.piramalswasthya.cho.R
+import org.piramalswasthya.cho.databinding.ActivityAddPatientBinding
+import org.piramalswasthya.cho.databinding.ActivityHomeBinding
+import org.piramalswasthya.cho.databinding.FragmentUsernameBinding
+import org.piramalswasthya.cho.ui.add_patient_activity.AddPatientActivity
+import org.piramalswasthya.cho.ui.login_activity.username.UsernameFragmentDirections
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+
+    private var _binding: ActivityHomeBinding? = null
+
+    private val binding: ActivityHomeBinding
+        get() = _binding!!
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+//    ): View {
+//        _binding = FragmentUsernameBinding.inflate(layoutInflater, container, false)
+//        return binding.root
+//    }
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, AddPatientActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
