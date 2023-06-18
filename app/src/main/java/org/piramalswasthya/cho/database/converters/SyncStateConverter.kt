4c5dd2fe3648081f817e9f0d34b3adb74dcbfb6f
pauldibyendu <paul.d@beehyv.com>
2023-06-18 05:56:43 -0700

    converters added

---
+++ app/src/main/java/org/piramalswasthya/cho/database/converters/SyncStateConverter.kt
@@ -0,0 +1,17 @@
+package org.piramalswasthya.sakhi.database.converters
+
+import androidx.room.TypeConverter
+import org.piramalswasthya.cho.database.room.SyncState
+
+object SyncStateConverter {
+
+    @TypeConverter
+    fun toInt(value: SyncState): Int {
+        return value.ordinal
+    }
+
+    @TypeConverter
+    fun fromInt(value : Int): SyncState {
+        return SyncState.values()[value]
+    }
+}
