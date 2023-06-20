5cdf25973274d75b4634a9a25d1c2a7385714082
pauldibyendu <paul.d@beehyv.com>
2023-06-20 04:04:33 -0700

    patient details activity added

---
+++ app/src/main/java/org/piramalswasthya/cho/ui/patient_details_activity/PatientDetailsActivity.kt
@@ -0,0 +1,12 @@
+package org.piramalswasthya.cho.ui.patient_details_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.piramalswasthya.cho.R
+
+class PatientDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_details)
+    }
+}
