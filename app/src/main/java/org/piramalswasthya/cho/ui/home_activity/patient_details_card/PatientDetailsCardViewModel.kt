59a16b394bcadc1031bd71822ebbd77684160acb
pauldibyendu <paul.d@beehyv.com>
2023-06-20 11:41:01 -0700

    Patient list added

--- app/src/main/java/org/piramalswasthya/cho/ui/home_activity/patient_details_card/PatientDetailsCardViewModel.kt
+++
@@ -1,7 +0,0 @@
-package org.piramalswasthya.cho.ui.home_activity.patient_details_card
-
-import androidx.lifecycle.ViewModel
-
-class PatientDetailsCardViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
