a0eae7424cd7add98bb918ff67e2ff848754be8c
pauldibyendu <paul.d@beehyv.com>
2023-06-23 04:40:33 -0700

    un commented changes pushed

--- app/src/main/java/org/piramalswasthya/cho/ui/edit_patient_details_activity/EditPatientDetailsActivity.kt
+++ app/src/main/java/org/piramalswasthya/cho/ui/edit_patient_details_activity/EditPatientDetailsActivity.kt
@@ -57,31 +57,6 @@ class EditPatientDetailsActivity: AppCompatActivity() {
         val fragment = PersonalDetailsFragment(patientDetails);
         supportFragmentManager.beginTransaction().replace(binding.patientDetalis.id, fragment).commit()
 
-//        binding.btnSubmit.setOnClickListener {
-//            when (currFragment){
-//                R.id.fragment_personal_details -> {
-//                    val fragmentVisitDetails = VisitDetailsFragment(patientDetails);
-//                    supportFragmentManager.beginTransaction().replace(binding.patientDetalis.id, fragmentVisitDetails).commit()
-//                    currFragment = R.id.fragment_visit_details
-//                }
-//                R.id.fragment_visit_details -> {
-//                    val fragmentVitalsForm = VitalsFormFragment(patientDetails);
-//                    supportFragmentManager.beginTransaction().replace(binding.patientDetalis.id, fragmentVitalsForm).commit()
-//                    currFragment = R.id.fragment_vitals_form
-//                }
-//                R.id.fragment_vitals_form -> {
-//                    val fragmentPrescription = PrescriptionFragment(patientDetails);
-//                    supportFragmentManager.beginTransaction().replace(binding.patientDetalis.id, fragmentPrescription).commit()
-//                    currFragment = R.id.fragment_prescription
-//                }
-//                R.id.fragment_prescription -> {
-//                    val fragmentPersonalDetails = PersonalDetailsFragment(patientDetails);
-//                    supportFragmentManager.beginTransaction().replace(binding.patientDetalis.id, fragmentPersonalDetails).commit()
-//                    currFragment = R.id.fragment_personal_details
-//                }
-//            }
-//        }
-
         binding.btnSubmit.setOnClickListener {
             when (currFragment){
                 R.id.fragment_personal_details -> {
@@ -140,54 +115,7 @@ class EditPatientDetailsActivity: AppCompatActivity() {
                 }
             }
         }
-
-//        // Populate the spinner with options
-//        genderSpinner()
-//
-//
-//
-//        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
-//        btnSubmit.setOnClickListener {
-//            // Call the method to retrieve and save the data
-//            getFormData()
-//        }
     }
-//    private fun genderSpinner(){
-//        val genderSpinner: Spinner = findViewById(R.id.spGender)
-//        val genderOptions = listOf("Select Gender", "Male", "Female", "Other")
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        genderSpinner.adapter = adapter
-//
-//        // Handle the user selection
-//        genderSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//                val selectedGender = genderOptions[position]
-//                if (selectedGender != "Select Gender") {
-//                    // Gender is selected, proceed with the chosen value
-//                } else {
-//                    // Prompt user to select a gender
-//                    Toast.makeText(this@EditPatientDetailsActivity, "Please select a gender", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//
-//            override fun onNothingSelected(parent: AdapterView<*>?) {
-//                // Handle the case when nothing is selected
-//            }
-//        })
-//    }
-//    private fun getFormData() {
-//        val firstName = findViewById<EditText>(R.id.etFirstName).text.toString()
-//        val lastName = findViewById<EditText>(R.id.etLastName).text.toString()
-//        val contactNo = findViewById<EditText>(R.id.etContactNo).text.toString()
-//        val gender = findViewById<Spinner>(R.id.spGender).selectedItem.toString()
-//        val age = findViewById<EditText>(R.id.etAge).text.toString()
-//
-//        viewModel.savePatientDetails(firstName, lastName, contactNo, gender, age)
-//
-//        // Display a toast message to indicate successful data submission
-//
-//    }
 
     override fun onDestroy() {
         super.onDestroy()
