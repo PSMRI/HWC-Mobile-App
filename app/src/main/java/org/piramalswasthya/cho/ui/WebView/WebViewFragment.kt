50b8f0abf7b080b939c2bb896cc6a6572f7313bb
Aryan Tiwari <aryan.t@beehyv.com>
2023-06-29 15:45:29 +0530

    web_view_commit

--- app/src/main/java/org/piramalswasthya/cho/ui/WebView/WebViewFragment.kt
+++ app/src/main/java/org/piramalswasthya/cho/ui/WebView/WebViewFragment.kt
@@ -1,60 +1,50 @@
 package org.piramalswasthya.cho.ui.WebView
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import org.piramalswasthya.cho.R
+import timber.log.Timber
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WebViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class WebViewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+@AndroidEntryPoint
+class WebViewFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val webUrl by lazy {
+        WebViewFragmentArgs.fromBundle(requireArguments()).webUrl
     }
-
+    private lateinit var webView : WebView
+    lateinit var btn : Button
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false)
+
+        var view : View = inflater.inflate(R.layout.fragment_web_view, container, false)
+        webView = view.findViewById(R.id.webview)
+
+        btn = view.findViewById(R.id.back_btn)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WebViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WebViewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Timber.d("here is the url: $webUrl")
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(webUrl)
+        btn.setOnClickListener{
+            findNavController().navigate(
+                WebViewFragmentDirections.backtopreviousFragment()
+            )
+        }
     }
 }
