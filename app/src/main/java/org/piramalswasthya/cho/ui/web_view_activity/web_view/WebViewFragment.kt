85b7e9c8869aefb37dae1c20507f1b25a9158c3d
waris <waris@beehyv.com>
2023-07-04 16:56:05 +0530

    resolved revisting eSanjeevani issue

--- app/src/main/java/org/piramalswasthya/cho/ui/web_view_activity/web_view/WebViewFragment.kt
+++ app/src/main/java/org/piramalswasthya/cho/ui/web_view_activity/web_view/WebViewFragment.kt
@@ -1,28 +1,23 @@
 package org.piramalswasthya.cho.ui.web_view_activity.web_view
 
+import android.app.ProgressDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.piramalswasthya.cho.R
-import org.piramalswasthya.cho.databinding.FragmentUsernameBinding
 import org.piramalswasthya.cho.databinding.FragmentWebViewBinding
-import org.piramalswasthya.cho.model.PatientDetails
-//import org.piramalswasthya.cho.ui.WebView.WebViewFragmentArgs
-import timber.log.Timber
 
 
 @AndroidEntryPoint
 class WebViewFragment constructor(
     private val webUrl: String,
 ): Fragment(R.layout.fragment_web_view) {
+    private lateinit var progressDialog: ProgressDialog
 
     private var _binding: FragmentWebViewBinding? = null
     private val binding: FragmentWebViewBinding
@@ -38,20 +33,35 @@ class WebViewFragment constructor(
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("here is the url","$webUrl")
-        Log.d("aaaaaaaaaaaa","adfdf")
+        progressDialog = ProgressDialog.show(activity, "Loading", "Loading eSanjeevani Please wait...", true)
+        progressDialog.setCancelable(false)
         binding.webview.settings.javaScriptEnabled = true
         binding.webview.settings.domStorageEnabled = true
+        binding.webview.settings.loadWithOverviewMode = true
+        binding.webview.settings.useWideViewPort = true
         binding.webview.webViewClient = WebViewClient()
+
+        binding.webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                progressDialog.show()
+                view.loadUrl(url)
+                return true
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                progressDialog.dismiss()
+            }
+        }
         binding.webview.loadUrl(webUrl)
-//        findNavController(){
-//
-//        }
-//        activity?.onBackPressedDispatcher()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("destryed", "fsdfdsfds");
+    override fun onDestroyView() {
+        // Destroy the WebView when the fragment view is destroyed
+         binding?.webview?.let {
+            it.stopLoading()
+            it.destroy()
+        }
+        super.onDestroyView()
     }
 }
