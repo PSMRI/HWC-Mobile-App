3e347f19b860693e9304e8f87e188b87b771190c
pauldibyendu <paul.d@beehyv.com>
2023-07-04 01:15:11 -0700

    more changes

--- app/src/main/java/org/piramalswasthya/cho/ui/web_view_activity/web_view/WebViewFragment.kt
+++ app/src/main/java/org/piramalswasthya/cho/ui/web_view_activity/web_view/WebViewFragment.kt
@@ -9,40 +9,49 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.piramalswasthya.cho.R
+import org.piramalswasthya.cho.databinding.FragmentUsernameBinding
+import org.piramalswasthya.cho.databinding.FragmentWebViewBinding
+import org.piramalswasthya.cho.model.PatientDetails
 //import org.piramalswasthya.cho.ui.WebView.WebViewFragmentArgs
 import timber.log.Timber
 
 
 @AndroidEntryPoint
-class WebViewFragment: Fragment(R.layout.fragment_web_view) {
+class WebViewFragment constructor(
+    private val webUrl: String,
+): Fragment(R.layout.fragment_web_view) {
 
-
-    private val webUrl by lazy {
-        WebViewFragmentArgs.fromBundle(requireArguments()).webUrl
-    }
-
-    private lateinit var webView : WebView
-    lateinit var btn : Button
+    private var _binding: FragmentWebViewBinding? = null
+    private val binding: FragmentWebViewBinding
+        get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var view : View = inflater.inflate(R.layout.fragment_web_view, container, false)
-//        webView = view.findViewById(R.id.webview)
-//
-//        btn = view.findViewById(R.id.back_btn)
-        return view
+        _binding = FragmentWebViewBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        Timber.d("here is the url: $webUrl")
-//        Log.d("aaaaaaaaaaaa","adfdf")
-//        webView.settings.javaScriptEnabled = true
-//        webView.webViewClient = WebViewClient()
-//        webView.loadUrl(webUrl)
+        Log.d("here is the url","$webUrl")
+        Log.d("aaaaaaaaaaaa","adfdf")
+        binding.webview.settings.javaScriptEnabled = true
+        binding.webview.settings.domStorageEnabled = true
+        binding.webview.webViewClient = WebViewClient()
+        binding.webview.loadUrl(webUrl)
+//        findNavController(){
+//
+//        }
+//        activity?.onBackPressedDispatcher()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("destryed", "fsdfdsfds");
     }
 }
