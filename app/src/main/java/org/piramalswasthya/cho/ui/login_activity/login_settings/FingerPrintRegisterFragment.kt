12ef8995a418afeb28fa67f5648c9d21b7eae59d
Babacar Ndiaye <babacar.n@beehyv.com>
2023-09-25 07:27:47 +0000

    Reduce apk size and remove fingerprint feature

--- app/src/main/java/org/piramalswasthya/cho/ui/login_activity/login_settings/FingerPrintRegisterFragment.kt
+++ app/src/main/java/org/piramalswasthya/cho/ui/login_activity/login_settings/FingerPrintRegisterFragment.kt
@@ -1,9 +1,9 @@
 package org.piramalswasthya.cho.ui.login_activity.login_settings
 
-import SecuGen.FDxSDKPro.JSGFPLib
-import SecuGen.FDxSDKPro.SGDeviceInfoParam
-import SecuGen.FDxSDKPro.SGFDxDeviceName
-import SecuGen.FDxSDKPro.SGFDxErrorCode
+//import SecuGen.FDxSDKPro.JSGFPLib
+//import SecuGen.FDxSDKPro.SGDeviceInfoParam
+//import SecuGen.FDxSDKPro.SGFDxDeviceName
+//import SecuGen.FDxSDKPro.SGFDxErrorCode
 import android.app.AlertDialog
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -45,7 +45,7 @@ class FingerPrintRegisterFragment: Fragment() {
     var userName: String? = null
     private var byteArrayData: ByteArray? = null
     private var base64String: String = ""
-    private lateinit var sgfplib: JSGFPLib
+//    private lateinit var sgfplib: JSGFPLib
     private var mPermissionIntent: PendingIntent? = null
     private val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
     private var initialPos = 0
@@ -139,99 +139,99 @@ class FingerPrintRegisterFragment: Fragment() {
 
 
     private fun initialiseDevice() {
-        val pendingFlags: Int = PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        mPermissionIntent = PendingIntent.getBroadcast(
-            activity, 0,
-            Intent(ACTION_USB_PERMISSION), pendingFlags
-        )
-        val usbManager: UsbManager = requireContext().getSystemService(Context.USB_SERVICE) as UsbManager
-        sgfplib = JSGFPLib(this.requireActivity(), usbManager)
-        sgfplib.Init( SGFDxDeviceName.SG_DEV_AUTO)
-        val usbDevice: UsbDevice? = sgfplib.GetUsbDevice()
-        if (usbDevice == null) {
-            val dlgAlert = AlertDialog.Builder(activity)
-            dlgAlert.setMessage("USB Device Not Found Please check your Connection")
-            dlgAlert.setTitle("USB Device Error")
-            dlgAlert.setPositiveButton("OK",
-                DialogInterface.OnClickListener { _, _ ->
-                    return@OnClickListener
-                }
-            )
-            dlgAlert.setCancelable(false)
-            dlgAlert.create().show()
-        }
-        else {
-            var usbPermissionRequested = false
-            var hasPermission: Boolean = sgfplib.GetUsbManager().hasPermission(usbDevice)
-            if (!hasPermission) {
-                if (!usbPermissionRequested) {
-                    sgfplib.GetUsbManager().requestPermission(usbDevice, mPermissionIntent)
-                    usbPermissionRequested = true
-                } else {
-                    //wait up to 20 seconds for the system to grant USB permission
-                    hasPermission = sgfplib.GetUsbManager().hasPermission(usbDevice)
-                    var i = 0
-                    while (!hasPermission && i <= 40) {
-                        ++i
-                        hasPermission = sgfplib.GetUsbManager().hasPermission(usbDevice)
-                        try {
-                            Thread.sleep(500)
-                        } catch (e: InterruptedException) {
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            }
-            if(hasPermission){
-                captureFingerprint()
-            }
-        }
-        sgfplib.AutoOnEnabled()
+//        val pendingFlags: Int = PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+//        mPermissionIntent = PendingIntent.getBroadcast(
+//            activity, 0,
+//            Intent(ACTION_USB_PERMISSION), pendingFlags
+//        )
+//        val usbManager: UsbManager = requireContext().getSystemService(Context.USB_SERVICE) as UsbManager
+//        sgfplib = JSGFPLib(this.requireActivity(), usbManager)
+//        sgfplib.Init( SGFDxDeviceName.SG_DEV_AUTO)
+//        val usbDevice: UsbDevice? = sgfplib.GetUsbDevice()
+//        if (usbDevice == null) {
+//            val dlgAlert = AlertDialog.Builder(activity)
+//            dlgAlert.setMessage("USB Device Not Found Please check your Connection")
+//            dlgAlert.setTitle("USB Device Error")
+//            dlgAlert.setPositiveButton("OK",
+//                DialogInterface.OnClickListener { _, _ ->
+//                    return@OnClickListener
+//                }
+//            )
+//            dlgAlert.setCancelable(false)
+//            dlgAlert.create().show()
+//        }
+//        else {
+//            var usbPermissionRequested = false
+//            var hasPermission: Boolean = sgfplib.GetUsbManager().hasPermission(usbDevice)
+//            if (!hasPermission) {
+//                if (!usbPermissionRequested) {
+//                    sgfplib.GetUsbManager().requestPermission(usbDevice, mPermissionIntent)
+//                    usbPermissionRequested = true
+//                } else {
+//                    //wait up to 20 seconds for the system to grant USB permission
+//                    hasPermission = sgfplib.GetUsbManager().hasPermission(usbDevice)
+//                    var i = 0
+//                    while (!hasPermission && i <= 40) {
+//                        ++i
+//                        hasPermission = sgfplib.GetUsbManager().hasPermission(usbDevice)
+//                        try {
+//                            Thread.sleep(500)
+//                        } catch (e: InterruptedException) {
+//                            e.printStackTrace()
+//                        }
+//                    }
+//                }
+//            }
+//            if(hasPermission){
+//                captureFingerprint()
+//            }
+//        }
+//        sgfplib.AutoOnEnabled()
 
     }
 
-    private fun captureFingerprint() {
-        sgfplib.OpenDevice(0)
-        val deviceInfo = SGDeviceInfoParam()
-        sgfplib.GetDeviceInfo(deviceInfo)
-        byteArrayData = ByteArray(deviceInfo.imageWidth * deviceInfo.imageHeight)
-        val quality: Long = 60
-        val timeout: Long = 10000
-        var result = sgfplib.GetImageEx(byteArrayData, timeout, quality)
-        if (result == SGFDxErrorCode.SGFDX_ERROR_NONE) {
-            binding.fpImageReg.setImageBitmap(toGrayscale(byteArrayData!!,deviceInfo))
-            binding.textViewFp.visibility = View.VISIBLE
-            binding.textViewFpNot.visibility = View.GONE
-            base64String = Base64.encodeToString(byteArrayData, Base64.DEFAULT)
-            fpList.add(FingerPrint(initialPos, userName, base64String, fingerType))
-            // closing device after taking the reading
-            sgfplib.CloseDevice()
-
-            initialPos++
-            if(initialPos < 4) {
-                binding.fingerDropdownVal.setText(fingerList[initialPos],false)
-                fingerType = fingerList[initialPos]
-               if(initialPos > 0) binding.enrollFp.text = "Next Finger"
-            }
-            else {
-                binding.enrollFp.visibility = View.GONE
-                binding.submitFP.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    private fun toGrayscale(mImageBuffer: ByteArray, deviceInfoParam: SGDeviceInfoParam): Bitmap? {
-        val bits = ByteArray(mImageBuffer.size * 4)
-        for (i in mImageBuffer.indices) {
-            bits[i * 4 + 2] = mImageBuffer[i]
-            bits[i * 4 + 1] = bits[i * 4 + 2]
-            bits[i * 4] = bits[i * 4 + 1] // Invert the source bits
-            bits[i * 4 + 3] = -1 // 0xff, that's the alpha.
-        }
-
-        val bmpGrayscale = Bitmap.createBitmap(deviceInfoParam.imageWidth, deviceInfoParam.imageHeight,
-            Bitmap.Config.ARGB_8888)
-        bmpGrayscale.copyPixelsFromBuffer(ByteBuffer.wrap(bits))
-        return bmpGrayscale
-    }
+//    private fun captureFingerprint() {
+//        sgfplib.OpenDevice(0)
+//        val deviceInfo = SGDeviceInfoParam()
+//        sgfplib.GetDeviceInfo(deviceInfo)
+//        byteArrayData = ByteArray(deviceInfo.imageWidth * deviceInfo.imageHeight)
+//        val quality: Long = 60
+//        val timeout: Long = 10000
+//        var result = sgfplib.GetImageEx(byteArrayData, timeout, quality)
+//        if (result == SGFDxErrorCode.SGFDX_ERROR_NONE) {
+//            binding.fpImageReg.setImageBitmap(toGrayscale(byteArrayData!!,deviceInfo))
+//            binding.textViewFp.visibility = View.VISIBLE
+//            binding.textViewFpNot.visibility = View.GONE
+//            base64String = Base64.encodeToString(byteArrayData, Base64.DEFAULT)
+//            fpList.add(FingerPrint(initialPos, userName, base64String, fingerType))
+//            // closing device after taking the reading
+//            sgfplib.CloseDevice()
+//
+//            initialPos++
+//            if(initialPos < 4) {
+//                binding.fingerDropdownVal.setText(fingerList[initialPos],false)
+//                fingerType = fingerList[initialPos]
+//               if(initialPos > 0) binding.enrollFp.text = "Next Finger"
+//            }
+//            else {
+//                binding.enrollFp.visibility = View.GONE
+//                binding.submitFP.visibility = View.VISIBLE
+//            }
+//        }
+//    }
+//
+//    private fun toGrayscale(mImageBuffer: ByteArray, deviceInfoParam: SGDeviceInfoParam): Bitmap? {
+//        val bits = ByteArray(mImageBuffer.size * 4)
+//        for (i in mImageBuffer.indices) {
+//            bits[i * 4 + 2] = mImageBuffer[i]
+//            bits[i * 4 + 1] = bits[i * 4 + 2]
+//            bits[i * 4] = bits[i * 4 + 1] // Invert the source bits
+//            bits[i * 4 + 3] = -1 // 0xff, that's the alpha.
+//        }
+//
+//        val bmpGrayscale = Bitmap.createBitmap(deviceInfoParam.imageWidth, deviceInfoParam.imageHeight,
+//            Bitmap.Config.ARGB_8888)
+//        bmpGrayscale.copyPixelsFromBuffer(ByteBuffer.wrap(bits))
+//        return bmpGrayscale
+//    }
 }
