b7e2056d363a611225ce3b1d09eb386ef574a626
Babacar Ndiaye <babacar.n@beehyv.com>
2023-09-29 13:57:38 +0000

    Change color on header and buttons on the app

---
+++ app/src/main/java/org/piramalswasthya/cho/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package org.piramalswasthya.cho.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.piramalswasthya.cho.ui.home.HomeFragment
+import org.piramalswasthya.cho.ui.home_activity.DashboardFragment
+
+class ViewPagerAdapter(supportFragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(supportFragmentManager, lifecycle) {
+
+    // declare arrayList to contain fragments and its title
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun getItemCount(): Int {
+        return  2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+       return if(position==0){
+            DashboardFragment()
+        }
+        else{
+            HomeFragment()
+        }
+    }
+}
