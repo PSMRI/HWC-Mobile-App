dfa4cb77223a73050d71b2aaef20e06fbf096bb9
ishaan.d <ishaan.d@beehyv.com>
2023-10-20 14:13:54 +0530

    ben card age unit - singular and plural re-added worker for sync of audit timestamp issue resolved for login, logout audit data

--- app/src/main/java/org/piramalswasthya/cho/model/fhir/SelectedOutreachProgram.kt
+++ app/src/main/java/org/piramalswasthya/cho/model/fhir/SelectedOutreachProgram.kt
@@ -36,10 +36,10 @@ data class SelectedOutreachProgram(
     val option: String?,
 
     @ColumnInfo(name = "logoutTimestamp")
-    val logoutTimestamp: String?,
+    val logoutTimeStamp: String?,
 
     @ColumnInfo(name = "loginTimestamp")
-    val loginTimestamp: String?,
+    val loginTimeStamp: String?,
 
     @ColumnInfo(name = "latitude")
     val latitude: Double?,
