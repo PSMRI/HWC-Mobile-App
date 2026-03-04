c265b4c2e78b0522833fd56a7a2d2919071d8258
DeepakKumar471 <kumar.deepak@bizbrolly.com>
2026-03-04 09:13:37 +0530

    Now it's working

--- app/src/main/java/org/piramalswasthya/cho/CHOApplication.kt
+++ app/src/main/java/org/piramalswasthya/cho/CHOApplication.kt
@@ -6,7 +6,10 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.HiltAndroidApp
+import org.piramalswasthya.cho.network.TokenRefreshHolder
+import org.piramalswasthya.cho.network.TokenRefreshProvider
 import org.piramalswasthya.cho.ui.home_activity.DemoDataStore
 import timber.log.Timber
 import javax.inject.Inject
@@ -44,7 +47,19 @@ class CHOApplication : Application(), Configuration.Provider {
             Timber.plant(Timber.DebugTree())
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        // Set token refresh provider so AuthRefreshInterceptor can refresh JWT when expired
+        try {
+            val entryPoint = EntryPointAccessors.fromApplication(this, TokenRefreshEntryPoint::class.java)
+            TokenRefreshHolder.provider = entryPoint.tokenRefreshProvider()
+        } catch (e: Exception) {
+            Timber.w(e, "CHOApplication: Could not set TokenRefreshProvider")
+        }
+    }
 
+    @dagger.hilt.EntryPoint
+    @dagger.hilt.InstallIn(dagger.hilt.components.SingletonComponent::class)
+    interface TokenRefreshEntryPoint {
+        fun tokenRefreshProvider(): TokenRefreshProvider
     }
 
     companion object {
